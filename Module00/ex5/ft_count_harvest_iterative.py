480adc4e7e6a08af9fa5048451bfbf75fd58a51b
unknown <aymanegouhmid2@gmail.com>
2026-05-15 23:07:55 +0100

    ..

---
+++ Module00/ex5/ft_count_harvest_iterative.py
@@ -0,0 +1,8 @@
+def ft_count_harvest_iterative():
+    
+    days = int(input("days until harvest:"))
+
+    for i in range(1, days + 1):
+        print("Day", i)
+    
+    print("Harvest time!")
---
+++ Module00/ex6/ft_garden_summary.py
@@ -0,0 +1,8 @@
+def ft_garden_summary():
+    
+    garden_name = input("Enter garden name: ")
+    plants_number = input("enter number of plants: ")
+
+    print("Garden:", garden_name)
+    print("Plants:", plants_number)
+    print("status: Growing well!")
